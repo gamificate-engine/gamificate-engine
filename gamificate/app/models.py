613a9163e4ebcf79309941a7589b162a7750344f
Henrique Pereira <pereirapalmeirahenrique@gmail.com>
2020-03-29 19:37:43 +0100

    Other DB Models added (#4)

--- gamificate/app/models.py
+++ gamificate/app/models.py
@@ -2,6 +2,7 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class Admin(UserMixin, db.Model):
     id_admin = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(32))
@@ -10,21 +11,18 @@ class Admin(UserMixin, db.Model):
     password = db.Column(db.String(128))
     realms = db.relationship('Realm', backref='author', lazy='dynamic')
     # falta premium
-    
+
     def setPassword(self, password):
         self.password = generate_password_hash(password)
-
 
     def checkPassword(self, password):
         return check_password_hash(self.password, password)
 
-
     def __repr__(self):
-        return '<Admin {}>'.format(self.email) 
+        return '<Admin {}>'.format(self.email)
 
     def get_id(self):
-           return (self.id_admin)
-
+        return (self.id_admin)
 
 
 class Realm(db.Model):
@@ -35,8 +33,7 @@ class Realm(db.Model):
     badges = db.relationship('Badge', backref='author', lazy='dynamic')
 
     def __repr__(self):
-        return '<Realm {}>'.format(self.name) 
-
+        return '<Realm {}>'.format(self.name)
 
 
 class Badge(db.Model):
@@ -45,8 +42,64 @@ class Badge(db.Model):
     xp = db.Column(db.Integer)
     required = db.Column(db.Integer)
     realm_id = db.Column(db.Integer, db.ForeignKey('realm.id_realm'))
-    # falta rewards
+    reward_id = db.Column(db.Integer, db.ForeignKey('reward.id_reward'), nullable=True)
 
+    def __repr__(self):
+        return '<Badge {}>'.format(self.name)
+
+class Reward(db.Model):
+    id_reward = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    desc = db.Column(db.String(255))
+
+    def __repr__(self):
+        return '<Reward {}>'.format(self.name)
+
+# intermediate table between User and Reward
+user_reward = db.Table('user_reward',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id_user'), primary_key=True),
+    db.Column('reward_id', db.Integer, db.ForeignKey('reward.id_reward'), primary_key=True),
+    db.Column('redeem_date', db.DateTime, nullable=True)
+)
+
+# intermediate table between User and Badge
+user_badge = db.Table('user_badge',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id_user'), primary_key=True),
+    db.Column('badge_id', db.Integer, db.ForeignKey('badge.id_badge'), primary_key=True),
+    db.Column('progress', db.Integer),
+    db.Column('finished', db.Boolean),
+    db.Column('finished_date', db.DateTime)
+)
+
+class User(db.Model):
+    id_user = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(254), unique=True)
+    total_xp = db.Column(db.Integer)
+    active = db.Column(db.Boolean)
+    level = db.Column(db.Integer)
+    realm_id = db.Column(db.Integer, db.ForeignKey('realm.id_realm'))
+    badges = db.relationship("Badge", secondary=user_badge)
+    rewards = db.relationship("Reward", secondary=user_reward)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+
+class Standings(db.Model):
+    realm_id = db.Column(db.Integer, db.ForeignKey('realm.id_realm'), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id_realm'), primary_key=True)
+    total_xp = db.Column(db.Integer)
+    total_badges = db.Column(db.Integer)
+
+
+class Level(db.Model):
+    id_level = db.Column(db.Integer, primary_key=True)
+    a = db.Column(db.Integer)
+    b = db.Column(db.Integer)
+    c = db.Column(db.Integer)
+    realm_id = db.Column(db.Integer, db.ForeignKey('realm.id_realm'))
+
+    def __repr__(self):
+        return '<Level {}>'.format(self.realm_id)
 
 
 # User-Loader Function
