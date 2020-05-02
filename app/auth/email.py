993d775d79005eb0d8d47dffb06f9cd3b516ddcc
Pedro Moreira <pedrombm19@gmail.com>
2020-05-02 02:01:22 +0100

    async email

--- app/auth/email.py
+++ app/auth/email.py
@@ -1,13 +1,6 @@
-from flask_mail import Message
-from app import app, mail
+from app import app
+from app.email import send_email
 from flask import render_template
-
-
-def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
-    mail.send(msg)
 
 
 def send_password_reset_email(admin):
@@ -15,7 +8,5 @@ def send_password_reset_email(admin):
     send_email('[Gamificate] Reset Your Password',
                sender=app.config['ADMINS'][0],
                recipients=[admin.email],
-               text_body=render_template('email/reset_password.txt',
-                                         admin=admin, token=token),
-               html_body=render_template('email/reset_password.html',
-                                         admin=admin, token=token))
+               text_body=render_template('email/reset_password.txt', admin=admin, token=token),
+               html_body=render_template('email/reset_password.html', admin=admin, token=token))
