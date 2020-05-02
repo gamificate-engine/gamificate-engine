850ea4014e1c9fc6940bc82eb8570e09abdebfa8
Henrique Pereira <pereirapalmeirahenrique@gmail.com>
2020-05-02 04:15:48 +0100

    rewards added to webapp

--- app/realms/badges/forms.py
+++ app/realms/badges/forms.py
@@ -1,16 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Optional
+from wtforms import StringField, IntegerField, SubmitField, SelectField
+from wtforms.validators import ValidationError, DataRequired, Optional, Length
 
 class BadgeForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    description = StringField('Description', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), Length(min=3, max=32)])
+    description = StringField('Description', validators=[DataRequired(), Length(min=3, max=256)])
     xp = IntegerField('XP', validators=[DataRequired()])
     required = IntegerField('XP required', validators=[DataRequired()])
-    image_url = StringField('Image URL', validators=[Optional()])
+    image_url = StringField('Image URL', validators=[Optional(), Length(min=3, max=2000)])
+    id_reward = SelectField('Choose a Reward', coerce=int, validators=[Optional()])
     submit = SubmitField('Create Badge')
 
-    
+    def add_rewards(self):
+        self.id_reward.choices = [(0, 'Choose a Reward')] + [(r.id_reward, r.name) for r in self.realm.rewards.all()]
 
     def validate_name(self, name):
         badges = self.realm.badges.all()
