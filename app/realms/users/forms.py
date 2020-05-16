468f526bf5096685c367622eb025be62cf50121b
Henrique Pereira <pereirapalmeirahenrique@gmail.com>
2020-05-16 15:47:27 +0100

    Web App - Edit and Delete Users (#76)
    * edit users
    
    * delete users
    
    * added delete modal
    
    * update form

--- app/realms/users/forms.py
+++ app/realms/users/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FileField
-from wtforms.validators import ValidationError, DataRequired, Email
+from wtforms import StringField, SubmitField, FileField, IntegerField, BooleanField
+from wtforms.validators import ValidationError, DataRequired, Email, Optional
 from flask import request
 from werkzeug.utils import secure_filename
 from app.realms.validation import validate_users_json
@@ -41,3 +41,23 @@ class JsonForm(FlaskForm):
 
         if not validate_users_json(json_obj):
             raise ValidationError('Wrong file structure.')
+
+class EditForm(FlaskForm):
+    id = IntegerField('ID', validators=[DataRequired()])
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    active = BooleanField('Active', validators=[Optional()])
+    submit = SubmitField('Edit User')
+
+
+    def validate_username(self, username):
+        users = self.realm.users.all()
+
+        if [u.username for u in users if u.username == username.data and u.id_user != self.id.data]:
+           raise ValidationError('Username already in use. Try again.')
+
+    def validate_email(self, email):
+        users = self.realm.users.all()
+
+        if [u.email for u in users if u.email == email.data and u.id_user != self.id.data]:
+            raise ValidationError('Email already in use. Try again.')
--- app/realms/users/routes.py
+++ app/realms/users/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import Admin, Realm, User
 from app.realms import bp
-from app.realms.users.forms import UserForm, JsonForm
+from app.realms.users.forms import UserForm, JsonForm, EditForm
 from app.realms.decorators import check_ownership, check_premium
 import os
 import json
@@ -19,8 +19,10 @@ def users(id):
     admin = Admin.query.get_or_404(current_user.get_id())
 
     form_json = JsonForm()
+    form_edit = EditForm()
+    form_edit.realm = realm
 
-    return render_template('realms/users/index.html', realm=realm, admin=admin, users=realm.users.all(), form_json=form_json)
+    return render_template('realms/users/index.html', realm=realm, admin=admin, users=realm.users.all(), form_json=form_json, form_edit=form_edit)
 
 
 @bp.route('/realms/<int:id>/users/new', methods=['GET', 'POST'])
@@ -100,6 +102,52 @@ def new_users_json(id):
     return redirect(url_for('realms.users', id=id))
 
 
+@bp.route('/realms/<int:id>/users/edit', methods=['POST'])
+@login_required
+@check_ownership
+def edit_user(id):
+    realm = Realm.query.get_or_404(id)
+    admin = Admin.query.get_or_404(current_user.get_id())
 
+    form = EditForm(request.form)
+    form.realm = realm
 
-# TODO: edit and show
+    if form.validate_on_submit():
+        user_id = form.id.data
+
+        infoUser = {'username' : form.username.data, 'email': form.email.data, 'active': form.active.data}
+
+        user = User.query.get_or_404(user_id)
+
+        if user.id_realm != id:
+            return render_template('errors/403.html'), 403
+        else:
+            user.from_dict(infoUser)
+            db.session.commit()
+            flash('User edited with success!')
+    
+    else:
+        flash('Something went wrong. Please try again.')
+
+    return redirect(url_for('realms.users', id=id))
+
+@bp.route('/realms/<int:id>/users/delete', methods=['POST'])
+@login_required
+@check_ownership
+def delete_user(id):
+    realm = Realm.query.get_or_404(id)
+    admin = Admin.query.get_or_404(current_user.get_id())
+
+    user_id = request.args.get('user', None)
+    if not user_id:
+        return render_template('errors/404.html'), 404
+
+    user = User.query.get_or_404(user_id)
+    if user.id_realm != id:
+        return render_template('errors/403.html'), 403
+    else:
+        db.session.delete(user)
+        db.session.commit()
+        flash('User deleted with success!')
+
+    return redirect(url_for('realms.users', id=id))
